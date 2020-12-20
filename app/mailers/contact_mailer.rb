236458d03cd6f97ad81d17a8639b390b4d0e4fa4
Kevin Liu <liukproductivity@gmail.com>
2020-12-20 01:48:30 +0000

    Added Mailer View

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'instructors@upskillcourses.com'
+    default to: 'liukproductivity@gmail.com'
     def contact_email(name, email, body)
         @name = name
         @email = email
