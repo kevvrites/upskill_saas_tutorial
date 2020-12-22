dfac38f519ae61daed97141cae4508ccffb4ea28
Kevin Liu <liukproductivity@gmail.com>
2020-12-22 06:02:29 +0000

    Added Mailer Setting to Environment

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'liukproductivity@gmail.com'
+  default to: 'liuksocial@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
